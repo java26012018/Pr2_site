bc2fee13ae917029adc4a11b810782f49ca4630d
java26012018 <contact.java.pro@gmail.com>
2018-04-18 21:43:11 +0300

    Add one empty string to Dao

--- Pr2_site/src/main/java/dao/Dao.java
+++ Pr2_site/src/main/java/dao/Dao.java
@@ -4,13 +4,14 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class Dao {
+class Dao {
+
     private static final String DRIVER = "com.mysql.jdbc.Driver";
     private static final String URL = "jdbc:mysql://contac01.mysql.ukraine.com.ua:3306/contac01_prilosh";
     private static final String LOGIN = "contac01_prilosh";
     private static final String PASS = "4h57futz";
 
-    protected static Connection getConnection() {
+    static Connection getConnection() {
         try {
             Class.forName(DRIVER);
         } catch (ClassNotFoundException ex) {
